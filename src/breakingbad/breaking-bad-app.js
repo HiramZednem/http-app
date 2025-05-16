3295b4a930b4a09cd92f05a6bd4c3376323456bc
Hiram Méndez <hirammendez000@gmail.com>
2025-05-15 21:49:54 -0600

    antes de richie

--- src/breakingbad/breaking-bad-app.js
+++ src/breakingbad/breaking-bad-app.js
@@ -4,18 +4,36 @@
 
 const fetchQuote = async () => {
     const res = await fetch(' https://api.breakingbadquotes.xyz/v1/quotes');
-    return await res.json();
+    const data = await res.json();
+    return data[0];
 }
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingBadApp = ( element ) => {
-    element.innerHTML = 'hola tonotos';
+export const BreakingBadApp = async ( element ) => {
 
-    console.log(fetchQuote())
+
+  
+
+          const {quote, author} = await fetchQuote();
     
+    element.innerHTML = `
+    <p>${quote}</p></br>
+    <p><small>${author}</small></p></br>
+    <button id='btn-quote'>obtener frase</button>
+    `;
+
+
+    document.querySelector('#btn-quote').addEventListener('click', async () => {
+          const {quote, author} = await fetchQuote();
+        element.innerHTML = `
+    <p>${quote}</p></br>
+    <p><small>${author}</small></p></br>
+    <button id='btn-quote'>obtener frase</button>
+    `;
+    })
 
 
 }
