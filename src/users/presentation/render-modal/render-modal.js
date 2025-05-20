17339e2f49a90558c48d8df514654a2c83a68eab
Hiram Méndez <hirammendez000@gmail.com>
2025-05-20 14:24:03 -0600

    [ADD]: functionality to modal

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -3,7 +3,16 @@ import './render-modal.css';
 
 
 let modal;
+let form;
 
+export const showModal = () => {
+    modal?.classList.remove('hide-modal');
+}
+
+export const hideModal = () => {
+    modal?.classList.add('hide-modal');
+    form?.reset()
+}
 /**
  * 
  * @param {HTMLDivElement} element 
@@ -16,6 +25,37 @@ export const renderModal = ( element ) => {
 
     modal.innerHTML = modalHTML;
     modal.className = 'modal-container hide-modal';
+
+    modal.addEventListener('click', (event) => {
+        if (event.target.classList.value !== 'modal-dialog') return;
+        hideModal();
+    })
+
+    form = modal.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+
+        const data = {};
+        for(const [key, value] of formData) {
+            if( key === 'balance' ) {
+                data[key] = Number(value);
+                continue;
+            }
+
+            if ( key === 'isActive' ) {
+                data[key] = true;
+                continue;
+            }
+
+            data[key] = value;
+        }
+        (data.isActive) ? data.isActive : data['isActive'] = false;
+        console.log(data);
+
+        hideModal();
+
+    })
 
     element.append(modal);
 }
